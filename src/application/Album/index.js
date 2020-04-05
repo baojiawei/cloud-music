c112d672132dd70c83a270386da10014586ad475
鲍佳玮 <bjw_0627@163.com>
2020-04-05 14:24:36 +0800

    关闭strictModel，实现Album动画

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container} from './style';
+import { CSSTransition } from 'react-transition-group'
 
 function Album (props) {
+  const [showStatus, setShowStatus] = useState (true);
   return (
+    <CSSTransition
+    in={showStatus}  
+    timeout={300} 
+    classNames="fly" 
+    appear={true} 
+    unmountOnExit
+    onExited={props.history.goBack}
+  >
     <Container>
-      66
     </Container>
+  </CSSTransition>
   )
 }
 
